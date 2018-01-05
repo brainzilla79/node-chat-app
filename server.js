a8b5ca52bce8c4e6f4e1ebcd0309c2a85e01dd26
Alex <alexanderhapp@me.com>
2018-01-05 12:10:31 -0800

    Add addMessages function

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000);
+app.use(express.static(__dirname));
+const server = app.listen(3000, () => {
+  console.log('server is listening on port', server.address().port);
+});
